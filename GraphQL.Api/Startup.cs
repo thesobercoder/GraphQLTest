e581451fbe2658b5c1c77d47ac8b1419451820cf
Soham Dasgupta <sohdasgupta@deloitte.com>
2020-01-06 17:28:41 +0530

    Add project files.

---
+++ GraphQL.Api/Startup.cs
@@ -0,0 +1,73 @@
+using GraphQL.EntityFramework;
+using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
+using GraphQL.Utilities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+
+namespace GraphQL.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+        public IConfiguration _Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<KestrelServerOptions>(o => { o.AllowSynchronousIO = true; });
+            services.Configure<IISServerOptions>(o => { o.AllowSynchronousIO = true; });
+            RegisterGraphQL(services);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseGraphQL<SchemaTest>();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseGraphQLPlayground(new GraphQLPlaygroundOptions
+                {
+                    Path = "/ui/playground",
+                    GraphQLEndPoint = "/graphql",
+                    PlaygroundSettings = new Dictionary<string, object>
+                    {
+                        ["editor.theme"] = "dark",
+                        ["tracing.hideTracingResponse"] = false
+                    }
+                });
+            }
+            app.UseHttpsRedirection();
+        }
+
+        private void RegisterGraphQL(IServiceCollection services)
+        {
+            GraphTypeTypeRegistry.Register<Customer, CustomerGraph>();
+            GraphTypeTypeRegistry.Register<Order, OrderGraph>();
+            services.AddDbContext<TestDBContext>(options => options.UseSqlServer(_Configuration.GetConnectionString("LeaseWebDB")));
+            EfGraphQLConventions.RegisterInContainer<TestDBContext>(
+                services,
+                model: TestDBContext.StaticModel
+            ); ;
+            EfGraphQLConventions.RegisterConnectionTypesInContainer(services);
+            services.AddSingleton<IDocumentExecuter, EfDocumentExecuter>();
+            services.AddScoped<CustomerGraph>();
+            services.AddScoped<OrderGraph>();
+            services.AddScoped<QueryTest>();
+            services.AddScoped<SchemaTest>();
+            services.AddGraphQL(o =>
+            {
+                o.ExposeExceptions = true;
+            });
+        }
+    }
+}
---
+++ GraphQL.Api/TestHarness.cs
@@ -0,0 +1,112 @@
+﻿using GraphQL.EntityFramework;
+using GraphQL.Types;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQL.Api
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int CustomerID { get; set; }
+        public ICollection<Customer> Customers { get; set; }
+    }
+
+    public class CustomerGraph : EfObjectGraphType<TestDBContext, Customer>
+    {
+        public CustomerGraph(IEfGraphQLService<TestDBContext> graphQlService) :
+            base(graphQlService)
+        {
+            Field(x => x.CustomerID);
+            Field(x => x.CustomerName);
+            AddNavigationListField(
+                name: "orders",
+                resolve: context => context.Source.Orders);
+        }
+    }
+
+    public class OrderGraph : EfObjectGraphType<TestDBContext, Order>
+    {
+        public OrderGraph(IEfGraphQLService<TestDBContext> graphQlService) :
+            base(graphQlService)
+        {
+            Field(x => x.OrderID);
+            Field(x => x.OrderDate);
+            AddNavigationListField(
+                name: "customers",
+                resolve: context => context.Source.Customers);
+        }
+    }
+
+    public class TestDBContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public TestDBContext(DbContextOptions<TestDBContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            if (builder != null)
+            {
+                builder.Entity<Customer>().ToTable("Customers");
+                builder.Entity<Customer>().HasKey(x => x.CustomerID);
+
+                builder.Entity<Order>().ToTable("Orders");
+                builder.Entity<Order>().HasKey(x => x.OrderID);
+
+                builder.Entity<Order>().HasMany(x => x.Customers);
+                builder.Entity<Customer>().HasMany(x => x.Orders);
+            }
+        }
+
+        static IModel BuildStaticModel()
+        {
+            var builder = new DbContextOptionsBuilder<TestDBContext>();
+            builder.UseSqlServer("Fake");
+            using var dbContext = new TestDBContext(builder.Options);
+            return dbContext.Model;
+        }
+
+        public static IModel StaticModel { get; } = BuildStaticModel();
+    }
+
+    public class SchemaTest : Schema
+    {
+        public SchemaTest(IServiceProvider resolver) : base(resolver)
+        {
+            Query = resolver.GetService<QueryTest>();
+        }
+    }
+
+    public class QueryTest : QueryGraphType<TestDBContext>
+    {
+        public QueryTest(IEfGraphQLService<TestDBContext> graphQlService) :
+            base(graphQlService)
+        {
+            Name = "Query";
+            AddQueryField(
+                name: "customers",
+                resolve: context => context.DbContext.Customers
+            );
+            AddQueryField(
+                name: "orders",
+                resolve: context => context.DbContext.Orders
+            );
+        }
+    }
+}
+
